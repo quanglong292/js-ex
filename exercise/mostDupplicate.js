9a272a50c5d0bd59b8e4c90750f3a6389386d666
quanglong292 <quang.long292@gmail.com>
2023-06-22 11:23:47 +0700

    maxProfit, mostDupplicate

---
+++ exercise/mostDupplicate.js
@@ -0,0 +1,3 @@
+// find most dupplicate element in array
+
+const findMostDupplicate = (array = [1, 3, 2, 8, 4, 9]) => {};
---
+++ leetcode/intermediate/maxProfit.js
@@ -0,0 +1,10 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day, and an integer fee representing a transaction fee.
+// Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+const maxProfit = (prices = [1,3,2,8,4,9], fee = 2) => {
+    for (let i = 0; i < prices.length; i++) {
+        const day = i
+        const price = prices[i]
+    }
+}
