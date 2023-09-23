978a44e38c1e15d917e3f15e118bbc89086dc21e
quanglong292 <quang.long292@gmail.com>
2023-09-23 18:34:05 +0700

    Finish implement quick sort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -24,9 +24,68 @@ const mergeSort = (array = [2, 3, 1, 4, 1, 3213]) => {
   const right = array.slice(splitNumber);
 
   const mergeSort = (left, right) => {};
-  const sort = (array) => {}
+  const sort = (array) => {};
 
   return mergeSort(sort(left), sort(right));
 };
 
-console.log(mergeSort());
+// console.log(mergeSort());
+
+const quickSort = (array) => {
+  const length = array.length;
+  if (!length || length === 1) return array;
+
+  const randomIndex = Math.floor(Math.random() * length);
+  const lastIndex = array.length - 1;
+  let pivot = array[randomIndex];
+  array[randomIndex] = array[lastIndex];
+  array[lastIndex] = pivot;
+
+  let left = []; // all elements that >= pivit
+  let right = []; // all elements that <= pivot
+
+  for (let index = 0; index < array.length - 1; index++) {
+    let leftBound = array[index];
+    let rightBound = array[array.length - 2 - index];
+
+    if (leftBound > pivot) {
+      left.push(leftBound);
+    } else {
+      right.push(rightBound);
+    }
+  }
+
+  return { array, left, right };
+};
+
+const quickSort2 = (arr, position) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+
+  console.log(`array ${position}: `, arr);
+  console.log("l | r", leftArr, rightArr);
+  // console.log("process: ", [...leftArr, pivot, ...rightArr]);
+
+  return [...quickSort2(leftArr, "left"), pivot, ...quickSort2(rightArr, "right")];
+};
+
+console.log(quickSort2([5, 1, 6, 4, 2, 3], "origin"));
+// (1). 6 - [5] - 1 4 2 3
+// (2). 6 - (5 - [1] - 4 2 3)
+// (3). 6 - (5 - (1 - (4 2 3)))
+// (3). 6 - (5 - (1 - (2 3 - [4] - ...))))
+// (3). 6 - (5 - (1 - ((... - [2] - 3) - 4 - ...))))
+// (3). 6 - (5 - (1 - ((... - [2] - 3) - 4 - ...))))
