1ad55f8ee2676d9cce9ba1ae9568a7565cbca3bf
quanglong292 <quang.long292@gmail.com>
2023-10-29 15:29:16 +0700

    pending-hackerrank: numberOfLineJump

---
+++ hackerrank/easy/numberOfLineJump.js
@@ -0,0 +1,13 @@
+const kangaroo = (x1, v1, x2, v2) => {
+  let kang1 = x1;
+  let kang2 = x2;
+  let step = 0;
+
+  if (x1 < x2 && v1 < v2) return "NO";
+
+  
+
+  return "YES";
+};
+
+console.log(kangaroo(21, 6, 47, 3));
